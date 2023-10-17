1849dc714378eab11c3fb1fba738498021568346
Loganrud <lrudsenske25@sandomenico.org>
2023-10-17 14:12:34 -0700

    2.2.7 Task 5 Started

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -11,6 +11,6 @@ public class MoveForward : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // To be implemented
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -2,21 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/***************************************
+ * Component of Spawn Manager, spawns animals into the scene but out of view.
+ * 
+ * Logan Rudsenske
+ * October 17, 2023 version 1.0
+ **************************************/
+
 public class SpawnManager : MonoBehaviour
 {
-    private float spawnRange;
-    private float startDelay = 2.0f;
-    private float spawnInterval = 1.5f;
-    [SerializeField]
-    private GameObject[] animals;
+    private float spawnRange;                    // distance to edge from center of field
+    private float startDelay = 2.0f;             // time before first spawn
+    private float spawnInterval = 1.5f;          // interval between spawns
+    [SerializeField] GameObject[] animals;       // list of animals to spawn
 
-
-
-    // Start is called before the first frame update
+    // Calls the SpawnRandomAnimal method after a certain delay, then every interval
     private void Start()
     {
         // To be implemented
     }
+
+    // Adds a random animal at a random point between the spawn ranges
     private void SpawnRandomAnimal()
     {
         // To be implemented
